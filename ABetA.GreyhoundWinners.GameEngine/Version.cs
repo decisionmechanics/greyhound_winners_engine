a3a99de8abe9e991110675d8055b4d5fcad55883
Andrew Tait <Andrew.Tait@decisionmechanics.com>
2024-10-02 11:36:26 +0100

    Add content to Version class

--- ABetA.GreyhoundWinners.GameEngine/Version.cs
+++ ABetA.GreyhoundWinners.GameEngine/Version.cs
@@ -2,5 +2,8 @@ namespace ABetA.GreyhoundWinners.GameEngine;
 
 public class Version
 {
+    /* Public fields */
     
+    // ReSharper disable once UnusedMember.Global
+    public const string Number = "1.0.0";
 }
