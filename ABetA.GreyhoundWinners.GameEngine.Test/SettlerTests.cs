2b5f790d7c7dc36fbc08a3a6535480994663c54f
Andrew Tait <Andrew.Tait@decisionmechanics.com>
2024-10-02 11:49:09 +0100

    Add unit test for settling Six Going Up market

--- ABetA.GreyhoundWinners.GameEngine.Test/SettlerTests.cs
+++ ABetA.GreyhoundWinners.GameEngine.Test/SettlerTests.cs
@@ -20,4 +20,21 @@ public class Tests
         Assert.That(result.Count(), Is.EqualTo(1));
         Assert.That(result.First().Selection, Is.EqualTo("Full Traps"));
     }
+    
+    [Test]
+    public void SettleCatchAMatchMarketWhenGivenASixGoingUpResultOnlySettlesASixGoingUpMarket()
+    {
+        // Arrange
+        
+        var settler = new Settler();
+        
+        // Act
+
+        var result = settler.SettleCatchAMatchMarket([1, 2, 3, 4, 5, 6]).ToList();
+
+        // Assert
+        
+        Assert.That(result.Count(), Is.EqualTo(1));
+        Assert.That(result.First().Selection, Is.EqualTo("Six Going Up"));
+    }
 }
